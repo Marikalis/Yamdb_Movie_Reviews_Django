83032be12ed0b48382b87c89e919ec3c1e93ccb7
Мария Лисицкая <lisitskayamari@gmail.com>
2022-05-06 13:08:08 +0100

    added default='localhost' allowed hosts

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default=' ')
 DEBUG = False
 
 # ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '62.84.120.222']
-ALLOWED_HOSTS = os.getenv('SERVER_NAMES').split(',')
+ALLOWED_HOSTS = os.getenv('SERVER_NAMES', default='localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
