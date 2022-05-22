949f9a1bc0cb1e924f0abf1517d9f7488cef32a1
Мария Лисицкая <lisitskayamari@gmail.com>
2022-05-22 23:16:44 +0100

    Added static

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -5,7 +5,7 @@ class TestSettings:
 
     def test_settings(self):
 
-        # assert not settings.DEBUG, 'Проверьте, что DEBUG в настройках Django выключен'
+        assert not settings.DEBUG, 'Проверьте, что DEBUG в настройках Django выключен'
         assert settings.DATABASES['default']['ENGINE'] == 'django.db.backends.postgresql', (
             'Проверьте, что используете базу данных postgresql'
         )
