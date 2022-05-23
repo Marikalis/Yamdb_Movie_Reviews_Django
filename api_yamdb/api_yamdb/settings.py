f144a0826851a93243310b65bd4889b993a288ff
Мария Лисицкая <lisitskayamari@gmail.com>
2022-05-23 15:37:20 +0100

    Env debug

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default=' ')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = os.getenv('SERVER_NAMES', default='localhost').split()
 
