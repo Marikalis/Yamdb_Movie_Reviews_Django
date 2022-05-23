29e028d61d280d0e02512fca752b4adc05e3fafb
Мария Лисицкая <lisitskayamari@gmail.com>
2022-05-23 15:07:48 +0100

    Added env touches

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default=' ')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('SERVER_NAMES', default='localhost').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
